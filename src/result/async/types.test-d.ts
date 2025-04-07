8e914d20cab8e321162b94209bc01ee6ca8e0a2e
oljimenez <oljimenez.prog@gmail.com>
2025-04-06 20:21:50 -0400

    feat: add unit type testing to async types helpers

---
+++ src/result/async/types.test-d.ts
@@ -0,0 +1,23 @@
+import { expectTypeOf, test } from "vitest";
+import type { InferAsyncOkTypes } from "./types";
+import type { ResultAsync } from "./result-async.builder";
+
+test("InferAsyncOkTypes should infer the Ok primitive values", () => {
+	type ResultExample = InferAsyncOkTypes<ResultAsync<number, boolean>>;
+
+	expectTypeOf<ResultExample>().toEqualTypeOf<number>();
+});
+
+test("InferAsyncOkTypes should infer Ok object values", () => {
+	type ResultOk = { name: string };
+	type ResultExample = InferAsyncOkTypes<ResultAsync<ResultOk, boolean>>;
+
+	expectTypeOf<ResultExample>().toEqualTypeOf<ResultOk>();
+});
+
+test("InferAsyncOkTypes should fail if wrong value is entered", () => {
+	type ResultOk = { name: string }[];
+	type ResultExample = InferAsyncOkTypes<ResultAsync<ResultOk, boolean>>;
+
+	expectTypeOf<ResultExample>().toEqualTypeOf<ResultOk>();
+});
